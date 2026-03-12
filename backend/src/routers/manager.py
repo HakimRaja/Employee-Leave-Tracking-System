d16a0560c6dd40a4bb003f2b2221f19d0bed46b2
HakimRaja <hakim.raja756@gmail.com>
2026-03-13 01:15:15 +0500

    add: controller func to router

--- backend/src/routers/manager.py
+++ backend/src/routers/manager.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends,HTTPException
 from src.database.connection import get_db_session
 from sqlmodel.ext.asyncio.session import AsyncSession
+from src.controllers.manager import (manage_leave_request as manage_leave_request_controller)
 
 app = APIRouter(prefix="/manager", tags=["manager operations"])
 
@@ -9,6 +10,6 @@ async def manage_leave_request(request_id: str, action: str, session: AsyncSessi
     try:
         if action not in ["approve", "reject"]:
             raise HTTPException(status_code=400, detail="Invalid action. Use 'approve' or 'reject'.")
-        return
+        return await manage_leave_request_controller(request_id, action, session)
     except HTTPException as e:
         raise
